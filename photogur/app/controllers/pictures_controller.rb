f018902a9ad46dcc64cce9d3fbfcb16681e6c35b
kevinberg3 <kevinberg3@gmail.com>
2013-06-26 11:59:40 -0400

    Allowing pictures to be showing within /pictures

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,6 +1,23 @@
 class PicturesController < ApplicationController
 
 	def index
-
+		@pictures = [
+			{
+				:title => "This is a title",
+				:artist => "This is the artist's name",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			},
+			{
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      },
+      {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+      }
+		]
+	 # match 'post/:pictures' => 'pictures#index', via: [:get, :post]
 	end
 end
--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -1,5 +1,8 @@
 Photogur::Application.routes.draw do
+  
   get 'pictures' => 'pictures#index'
+
+end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -54,4 +57,4 @@ Photogur::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
