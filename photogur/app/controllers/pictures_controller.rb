34a2aaafc049f726aa7811e755c5c5721a948817
kevinberg3 <kevinberg3@gmail.com>
2013-06-26 12:49:13 -0400

    Giving each picture its own URL using static routes

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -18,6 +18,31 @@ class PicturesController < ApplicationController
         :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
       }
 		]
-	 # match 'post/:pictures' => 'pictures#index', via: [:get, :post]
 	end
+
+	def picture0
+			@picture = {
+				:title => "This is a title",
+				:artist => "This is the artist's name",
+				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+			}
+			
+	end
+	 # match 'post/:pictures' => 'pictures#index', via: [:get, :post]
+	
+	 def picture1
+	 		@picture = {
+        :title  => "Sea Power",
+        :artist => "Stephen Scullion",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+      }
+	 	end
+
+	 def picture2
+	 		@picture = {
+        :title  => "Into the Poppies",
+        :artist => "John Wilhelm",
+        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+     	}
+	 	end
 end
