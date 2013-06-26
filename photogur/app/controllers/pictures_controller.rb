d5d56e5bf5ac8666259fa87ff18c34c7986a2fc0
kevinberg3 <kevinberg3@gmail.com>
2013-06-26 13:09:49 -0400

    Refactoring static routes to make them dynamic

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,5 +1,7 @@
 class PicturesController < ApplicationController
 
+	
+
 	def index
 		@pictures = [
 			{
@@ -20,29 +22,32 @@ class PicturesController < ApplicationController
 		]
 	end
 
-	def picture0
-			@picture = {
-				:title => "This is a title",
-				:artist => "This is the artist's name",
-				:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
-			}
-			
-	end
-	 # match 'post/:pictures' => 'pictures#index', via: [:get, :post]
-	
-	 def picture1
-	 		@picture = {
-        :title  => "Sea Power",
-        :artist => "Stephen Scullion",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
-      }
-	 	end
-
-	 def picture2
-	 		@picture = {
-        :title  => "Into the Poppies",
-        :artist => "John Wilhelm",
-        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
-     	}
-	 	end
+	@picture = @pictures[params[:id].to_i]
 end
+
+	# def picture0
+	# 		@picture = {
+	# 			:title => "This is a title",
+	# 			:artist => "This is the artist's name",
+	# 			:url => "http://monicao.s3.amazonaws.com/bitmaker/house.jpg"
+	# 		}
+			
+	# end
+	#  # match 'post/:pictures' => 'pictures#index', via: [:get, :post]
+	
+	#  def picture1
+	#  		@picture = {
+ #        :title  => "Sea Power",
+ #        :artist => "Stephen Scullion",
+ #        :url    => "http://monicao.s3.amazonaws.com/bitmaker/wave.jpg"
+ #      }
+	#  	end
+
+	#  def picture2
+	#  		@picture = {
+ #        :title  => "Into the Poppies",
+ #        :artist => "John Wilhelm",
+ #        :url    => "http://monicao.s3.amazonaws.com/bitmaker/girl.jpg"
+ #     	}
+	#  	end
+
--- photogur/config/routes.rb
+++ photogur/config/routes.rb
@@ -2,9 +2,7 @@ Photogur::Application.routes.draw do
   
   get 'pictures' => 'pictures#index'
 
-  get 'pictures/0' => 'pictures#picture0'
-  get 'pictures/1' => 'pictures#picture1'
-  get 'pictures/2' => 'pictures#picture2'
+  get 'pictures/:id' => 'pictures#show', as "picture"
 
 end
   # The priority is based upon order of creation: first created -> highest priority.
