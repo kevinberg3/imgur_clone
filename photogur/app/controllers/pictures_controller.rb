f724ec778fa79f72db98ebc9c7f3522d838e0784
kevinberg3 <kevinberg3@gmail.com>
2013-06-27 18:48:29 -0400

    Commit of full functionality including edit, update and delete functions

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -21,7 +21,28 @@ class PicturesController < ApplicationController
 		end
 	end
 
-	
+	def edit
+		@picture = Picture.find(params[:id])
+	end
+
+	def update
+		@picture = Picture.find(params[:id])
+
+		if @picture.update_attributes(picture_params)
+			redirect_to "/pictures/#{@picture.id}"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@picture = Picture.find(params[:id])
+		@picture.destroy
+		redirect_to pictures_url
+	end
+
+
+	private
 	def picture_params
 			params.require(:picture).permit(:artist, :title, :url)
 	end
