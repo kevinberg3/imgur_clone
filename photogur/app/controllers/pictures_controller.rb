318d716b629fb5dc7c8dbb421651081727d8b315
kevinberg3 <kevinberg3@gmail.com>
2013-06-27 16:14:41 -0400

    refactoring view to use helpers and adding navigation

--- photogur/app/controllers/pictures_controller.rb
+++ photogur/app/controllers/pictures_controller.rb
@@ -1,20 +1,30 @@
 class PicturesController < ApplicationController
 
 	def index
-		@pictures = Picture.all
+		@picture = Picture.all
 	end
 
 	def show
-		@pictures = Picture.find(params[:id])
+		@picture = Picture.find(params[:id])
 	end
 
 	def new 
-		# @picture = @pictures[params[:id].to_i]
+		@picture = Picture.new
 	end
 
 	def create
-		# render :text => "Saving a picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
-
-		render :text => "Saving picture. URL: #{params[:url]}, Title: #{params[:title]}, Artist: #{params[:artist]}"
+		@picture = Picture.new(params[:picture])
+		# @picture = Picture.new(picture_params)
+		if @picture.save
+				redirect_to pictures_url
+		else
+				render :new
+		end
 	end
+
+	private
+	def picture_params
+			params.require(:picture).permit(:artist, :title, :url)
+	end
+
 end
--- photogur/db/migrate/20130627154201_create_pictures.rb
+++ photogur/db/migrate/20130627154201_create_pictures.rb
@@ -2,6 +2,7 @@ class CreatePictures < ActiveRecord::Migration
   def change
     create_table :pictures do |t|
       t.string :artist
+      t.string :title
       t.string :url
 
       t.timestamps
